e8f2acc5d7cdbef45935ceb5761b91078a74d58b
onlynight <zhang910416>
2016-01-13 19:33:45 -0800

    删除sample

--- app/src/androidTest/java/com/github/lion/photofliter/ApplicationTest.java
+++ app/src/androidTest/java/com/github/lion/photofliter/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.github.lion.photofilter.sample;
+package com.github.lion.photofliter;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/test/java/com/github/lion/photofliter/ExampleUnitTest.java
+++ app/src/test/java/com/github/lion/photofliter/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.github.lion.photofilter.sample;
+package com.github.lion.photofliter;
 
 import org.junit.Test;
 
